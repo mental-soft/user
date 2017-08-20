12320e402ecbad06fa7ab50525bc6ef8e7a79ac1
hcguler <cihad_guler@hotmail.com>
2017-08-20 16:20:44 +0300

    #USER-12 fix application test

--- src/test/java/com/teammental/user/ApplicationTest.java
+++ src/test/java/com/teammental/user/ApplicationTest.java
@@ -1,9 +1,16 @@
 package com.teammental.user;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 /**
  * Created by hcguler on 7/8/2017.
  */
@@ -13,13 +20,19 @@ import org.springframework.test.context.junit4.SpringRunner;
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class ApplicationTest {
+  @Autowired
+  private MockMvc mvc;
+
   /**
-   * Index sayfası testi.
-   *
-   * @throws Exception exception
+   * test metodudur.
+   * @throws Exception
    */
   @Test
-  public void contextLoads() throws Exception {
+  public void getIndex() throws Exception {
+    mvc.perform(MockMvcRequestBuilders.get("/")
+        .accept(MediaType.TEXT_HTML))
+        .andExpect(status().isOk());
   }
 }
