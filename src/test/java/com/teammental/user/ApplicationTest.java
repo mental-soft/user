f434a88c9db469fa7ef4c7a495c18be3af6c65c6
hcguler <cihad_guler@hotmail.com>
2017-08-20 16:31:34 +0300

    #USER-12 fix application test

--- src/test/java/com/teammental/user/ApplicationTest.java
+++ src/test/java/com/teammental/user/ApplicationTest.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -19,8 +20,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
  * Test classı.
  */
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest(HelloController.class)
 public class ApplicationTest {
   @Autowired
   private MockMvc mvc;
